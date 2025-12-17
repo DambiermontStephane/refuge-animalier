2c829246d9ed601e331b614af65bbf8964106fb7
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2025-12-17 11:14:21 +0100

    added en/fr traduction

---
+++ lang/en/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'informations' => 'Informations',
+    'schedule' => 'Schedule',
+    'monday_to_friday' => 'Monday to friday',
+    'saturday' => 'Saturday'
+];
---
+++ lang/fr/footer.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'informations' => 'Informations',
+    'schedule' => 'Horaires',
+    'monday_to_friday' => 'Du lundi au vendredi',
+    'saturday' => 'Samedi'
+];
