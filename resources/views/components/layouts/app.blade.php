d0bac0b0c1a8e6743f5d06c5d585c03270f04166
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2025-12-17 10:52:36 +0100

    implemented index component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,25 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
+<!doctype html>
+<html lang="{!! app()->getLocale() !!}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="author" content="Dambiermont Stéphane">
+        <meta name="description" content="{{ __('headings.meta_description') }}">
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+        <title>{{ config('app.name') }}</title>
+    </head>
+    <body>
+    <header>
+        <h1 class="sr-only">{{ __('headings.title') }}</h1>
+        <x-nav/>
+    </header>
+    <main>
         {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+    </main>
+        <x-footer.layout/>
+    </body>
+</html>
