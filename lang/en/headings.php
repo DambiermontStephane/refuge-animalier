c2adee06e9bcf3ba4cd79676d37f0125033aee4e
DambiermontStephane <stephane.dambiermont@student.hepl.be>
2025-12-17 10:34:34 +0100

    added fr/en traduction

---
+++ lang/en/headings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Happy Paws',
+    'adoption' => 'Adoption',
+    'volunteer' => 'Volunteering',
+    'about' => 'About us',
+    'contact' => 'Contact',
+    'meta_description' => 'Website representing an animal shelter, Happy Paws',
+];
---
+++ lang/fr/headings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'Les Pattes Heureuses',
+    'adoption' => 'Adoption',
+    'volunteer' => 'Bénévolat',
+    'about' => 'À propos',
+    'contact' => 'Contact',
+    'meta_description' => 'Site web représentant un refuge animalier, Les Pattes Heureuses',
+];
